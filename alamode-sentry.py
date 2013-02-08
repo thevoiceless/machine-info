ea3916b935f96d272f0dda8ae3c9778e9e56b87a
Riley Moses <victi.resurgo@gmail.com>
2013-02-07 20:16:51 -0700

    Run all commands, generate not-very-readable file

--- alamode-sentry.py
+++ alamode-sentry.py
@@ -10,7 +10,7 @@
 
 # Processor:
 #	Name: cat /proc/cpuinfo | grep "model name" | uniq
-#	Speed: cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_max_freq | uniq
+#	Speed (Hz): cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_max_freq | uniq
 #	Number of real cores: cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $4}'
 #	Number of virtual cores: cat /proc/cpuinfo | grep "^processor" | wc -l
 #	Avg. load for the last...
@@ -39,7 +39,26 @@ import os
 
 # Constants for SSH and commands to be executed
 SSH_HOSTNAME_PREFIX = "rimoses@"
-COMMAND_NUM_USERS = "users | wc -w;"
+INFO_COMMANDS = {
+	'num_users': "echo num_users=`users | wc -w`;",
+	'num_processes': "echo num_processes=`ps -A --no-headers | wc -l`;",
+	'num_running_processes': "ps axo state | grep R | wc -l | awk '{print \"num_running_processes=\" $1}';",
+	'num_sleeping_processes': "ps axo state | grep S | wc -l | awk '{print \"num_sleeping_processes=\" $1}';",
+	'num_stopped_processes': "ps axo state | grep T | wc -l | awk '{print \"num_stopped_processes=\" $1}';",
+	'num_zombie_processes': "ps axo state | grep Z | wc -l | awk '{print \"num_zombie_processes=\" $1}';",
+	'processor_name': "echo processor_name=`cat /proc/cpuinfo | grep \"model name\" | uniq | sed s/^.*:\ *//`;",
+	'processor_speed': "echo processor_speed=`cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_max_freq | uniq | awk '{print $1/1000000 }'` GHz;",
+	'processor_num_real_cores': "echo processor_num_real_cores=`cat /proc/cpuinfo | grep \"cpu cores\" | uniq | awk '{print $4}'`;",
+	'processor_num_virt_cores': "echo processor_num_virt_cores=`cat /proc/cpuinfo | grep \"^processor\" | wc -l`;",
+	'avg_load_1min': "echo avg_load_1min=`uptime | awk '{print $10}' | sed s/,//`;",
+	'avg_load_5min': "echo avg_load_5min=`uptime | awk '{print $11}' | sed s/,//`;",
+	'avg_load_15min': "echo avg_load_15min=`uptime | awk '{print $12}' | sed s/,//`;",
+	'cpu_utilization': "echo cpu_utilization=`top -b -n 1 | grep ^Cpu | sed s/^.*:\ *//`;",
+	'cache_types': "let levels=`ls /sys/devices/system/cpu/cpu0/cache/ | wc -w`; for ((l=0; l<$levels; l++)); do let n=$l+1; echo \"lvl${n}_type=`cat /sys/devices/system/cpu/cpu0/cache/index${l}/type | sed s/K/\ kB/`\"; done;",
+	'cache_sizes': "let levels=`ls /sys/devices/system/cpu/cpu0/cache/ | wc -w`; for ((l=0; l<$levels; l++)); do let n=$l+1; echo \"lvl${n}_size=`cat /sys/devices/system/cpu/cpu0/cache/index${l}/size | sed s/K/\ kB/`\"; done;",
+	'memory_total': "cat /proc/meminfo | grep MemTotal | awk '{print \"memory_total=\" $2 " " $3}' | sed s/kB/\ kB/;",
+	'memory_free': "cat /proc/meminfo | grep MemFree | awk '{print \"memory_free=\" $2 " " $3}' | sed s/kB/\ kB/;",
+	'interrupt_data': "echo --START_INTERRUPT_TABLE--; cat /proc/interrupts; echo --END_INTERRUPT_TABLE--;" }
 
 # Directory in which to save info
 infoDir = ""
@@ -48,9 +67,14 @@ infoDir = ""
 def getInfo(hostname):
 	with open(hostname, "w") as hostInfoFile:
 		command = "ssh {}".format(SSH_HOSTNAME_PREFIX + hostname).split(" ")
-		command.append(COMMAND_NUM_USERS)
-		print command
+		for info, theCommandToGetTheInfo in INFO_COMMANDS.iteritems():
+			command.append(theCommandToGetTheInfo)
+		# command.append(COMMAND_NUM_USERS)
+		# command.append(COMMAND_NUM_PROCESSES)
+		# print command
 		# p = subprocess.Popen(command)
+		# p = subprocess.Popen(command, stdout=subprocess.PIPE)
+		# print p.communicate()[0]
 		hostInfoFile.write(subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0])
 
 # Command-line arguments
