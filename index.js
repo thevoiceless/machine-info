71c8cb013dda01c5caffba14fcb87160c1bcb4b8
Riley Moses <victi.resurgo@gmail.com>
2013-02-14 18:58:44 -0700

    Show tooltips for processes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	$('tr#cpu-row').tooltip({'title':'Click to show/hide details','delay':100});
+	$('tr#cpu-row').hover(
+		function() {
+			$(this).css('background-color', '#EDEDED')
+		},
+		function() {
+			$(this).css('background-color', 'white')
+		});
 	$('tr#cpu-row').click(
 		function() {
 			$('tr.hidden.cpu').each(
@@ -7,7 +14,15 @@ $(document).ready(function() {
 					$(this).fadeToggle()
 				})
 	});
+
 	$('tr#mem-row').tooltip({'title':'Click to show/hide details','delay':100});
+	$('tr#mem-row').hover(
+		function() {
+			$(this).css('background-color', '#EDEDED')
+		},
+		function() {
+			$(this).css('background-color', 'white')
+		});
 	$('tr#mem-row').click(
 		function() {
 			$('tr.hidden.mem').each(
@@ -15,4 +30,25 @@ $(document).ready(function() {
 					$(this).fadeToggle()
 				})
 	});
+
+	$('#running-procs-bar').tooltip({'title':'Running','placement':'bottom'})
+	$('#sleeping-procs-bar').tooltip({'title':'Sleeping','placement':'bottom'})
+	$('#stopped-procs-bar').tooltip({'title':'Stopped','placement':'bottom'})
+	$('#zombie-procs-bar').tooltip({'title':'Zombie','placement':'bottom'})
+
+	$('tr#proc-row').tooltip({'title':'Click to show/hide details','delay':100})
+	$('tr#proc-row').hover(
+		function() {
+			$(this).css('background-color', '#EDEDED')
+		},
+		function() {
+			$(this).css('background-color', 'white')
+		});
+	$('tr#proc-row').click(
+		function() {
+			$('tr.hidden.proc').each(
+				function() {
+					$(this).fadeToggle()
+				})
+		});
 });
