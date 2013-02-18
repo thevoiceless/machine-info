f1b7ffb5f860ee22926c9093b103f6932c5ef37a
Riley Moses <victi.resurgo@gmail.com>
2013-02-17 22:29:04 -0700

    Collapse information to just machine name by default

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-	$('tr#cpu-row').tooltip({'title':'Click to show/hide details','delay':100});
+	$('.page-header').tooltip({'title':'Click to show/hide machine information','delay':100});
+
+	var $elements = $('.page-header, .machine-info');
+
+	$('.page-header').click(
+		function() {
+			$elements.eq($elements.index(this) + 1).fadeToggle();
+		});
+
+	// $('tr#cpu-row').tooltip({'title':'Click to show/hide details','delay':100});
 	$('tr#cpu-row').hover(
 		function() {
 			$(this).css('background-color', '#EDEDED')
@@ -15,7 +24,7 @@ $(document).ready(function() {
 				})
 	});
 
-	$('tr#mem-row').tooltip({'title':'Click to show/hide details','delay':100});
+	// $('tr#mem-row').tooltip({'title':'Click to show/hide details','delay':100});
 	$('tr#mem-row').hover(
 		function() {
 			$(this).css('background-color', '#EDEDED')
@@ -36,7 +45,7 @@ $(document).ready(function() {
 	$('#stopped-procs-bar').tooltip({'title':'Stopped','placement':'bottom'})
 	$('#zombie-procs-bar').tooltip({'title':'Zombie','placement':'bottom'})
 
-	$('tr#proc-row').tooltip({'title':'Click to show/hide details','delay':100})
+	// $('tr#proc-row').tooltip({'title':'Click to show/hide details','delay':100})
 	$('tr#proc-row').hover(
 		function() {
 			$(this).css('background-color', '#EDEDED')
